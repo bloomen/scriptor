b04a2d254d239279371090459d52d6d7df9ef262
Christian Blume <chr.blume@gmail.com>
2023-02-02 21:37:05 +1300

    improve client

--- clients/client.py
+++ clients/client.py
@@ -6,33 +6,43 @@ class ScriptorHandler(logging.Handler):
 
     def __init__(self, socket_file, channel_name, level=logging.NOTSET):
         super().__init__(level)
-        self.table = str.maketrans({
+        self._table = str.maketrans({
             "<": "&lt;",
             ">": "&gt;",
             "&": "&amp;",
             "'": "&apos;",
             '"': "&quot;",
         })
-        self.channel_name = self.xmlesc(channel_name)
-        self.sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-        self.sock.connect(socket_file)
+        self._channel_name = self._xmlesc(channel_name)
+        self._socket_file = socket_file
+        self._connect()
 
-    def xmlesc(self, data):
-        return data.translate(self.table)
+    def _connect(self):
+        try:
+            self._sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+            self._sock.connect(self._socket_file)
+        except BaseException:
+            pass
+
+    def _xmlesc(self, data):
+        return data.translate(self._table)
 
     def emit(self, record):
         msg = ("<c>{channel_name}</c><s>{seconds_since_epoch}</s>"
                "<l>{log_level}</l><p>{process}</p><f>{filename}</f>"
                "<i>{line_no}</i><n>{function_name}</n><m>{log_message}</m>"
-               ).format(channel_name=self.channel_name,
-                       seconds_since_epoch=record.created,
-                       log_level=int(record.levelno / 10),
-                       process=record.process,
-                       filename=self.xmlesc(record.filename),
-                       line_no=record.lineno,
-                       function_name=self.xmlesc(record.funcName),
-                       log_message=self.xmlesc(record.msg))
-        self.sock.send(msg.encode())
+               ).format(channel_name=self._channel_name,
+                        seconds_since_epoch=record.created,
+                        log_level=int(record.levelno / 10),
+                        process=record.process,
+                        filename=self._xmlesc(record.filename),
+                        line_no=record.lineno,
+                        function_name=self._xmlesc(record.funcName),
+                        log_message=self._xmlesc(record.msg))
+        try:
+            self._sock.send(msg.encode())
+        except BaseException:
+            self._connect()
 
 
 logger = logging.getLogger(__name__)
