302bad2489e6d4ebbc7a204d712ee96533ceb26e
Christian Blume <chr.blume@gmail.com>
2023-02-05 20:28:00 +1300

    fix warning

--- src/aio.h
+++ src/aio.h
@@ -4,8 +4,13 @@
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wconversion"
 #pragma clang diagnostic ignored "-Wdeprecated-declaration"
+#elif defined(__GNUC__)
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wconversion"
 #endif
 #include <asio.hpp>
 #if defined(__clang__)
 #pragma clang diagnostic pop
+#elif defined(__GNUC__)
+#pragma GCC diagnostic pop
 #endif
